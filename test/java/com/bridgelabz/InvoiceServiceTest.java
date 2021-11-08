aed164bc5efd86ed92b23628164a63c50f97522e
suresh Daneti <sureshdaneti99@gmail.com>
2021-11-08 21:09:57 +0530

    Added: Given a user id, the invoice service gets the list of rides from the ride repository and return the invoice

--- test/java/com/bridgelabz/InvoiceServiceTest.java
+++ test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -44,6 +44,14 @@ public class InvoiceServiceTest {
 		Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
 	}
 
+	@Test
+	public void givenUserId_ShouldReturnInvoiceSummary() {
+		InvoiceService invoiceService = new InvoiceService();
+		InvoiceSummary invoiceSummary = invoiceService.getInvoice(1);
+		InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30);
+		Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+	}
+
 }
 
 
